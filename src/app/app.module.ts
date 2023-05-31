4283c70e03dfd04ca8448ff45af39b5dcf649e88
Jesús Sebastián Jaime Oviedo <a01412442@tec.mx>
2023-05-31 08:34:24 -0600

    primer setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoListComponent } from './components/photo-list/photo-list.component';
+import { PhotoFormComponent } from './components/photo-form/photo-form.component';
+import { PhotoPreviewComponent } from './components/photo-preview/photo-preview.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoListComponent,
+    PhotoFormComponent,
+    PhotoPreviewComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/photo-preview/photo-preview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-preview',
+  templateUrl: './photo-preview.component.html',
+  styleUrls: ['./photo-preview.component.css']
+})
+export class PhotoPreviewComponent {
+
+}
